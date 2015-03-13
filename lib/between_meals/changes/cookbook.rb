358534d19bf4c4c0929fec4afd4df79f5f1c54f9
Marcin Sawicki <odcinek@fb.com>
2015-03-13 15:38:46 -0700

    Style check

--- lib/between_meals/changes/cookbook.rb
+++ lib/between_meals/changes/cookbook.rb
@@ -1,13 +1,13 @@
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
 
 # Copyright 2013-present Facebook
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,10 +54,10 @@ module BetweenMeals
         # otherwise it was modified
         #   and will be re-uploaded
         if files.
-          select { |x| x[:status] == :deleted }.
-          map { |x| x[:path].match(%{.*metadata\.rb$}) }.
-          compact.
-          any?
+           select { |x| x[:status] == :deleted }.
+           map { |x| x[:path].match(%{.*metadata\.rb$}) }.
+           compact.
+           any?
           @status = :deleted
         else
           @status = :modified
@@ -78,7 +78,7 @@ module BetweenMeals
           g = self.explode_path(x[:path], cookbook_dirs)
           g[:cookbook_dir] + '/' + g[:name] if g
         end.
-        map do |_, change|
+          map do |_, change|
           # Confirm we're dealing with a cookbook
           # Changes to OWNERS or other stuff that might end up
           # in [core, other, secure] dirs are ignored
