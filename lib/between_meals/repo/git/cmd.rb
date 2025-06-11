1709705fa229b0eda775f504e159e16a4699b00a
dcrosby <dcrosby@meta.com>
2025-06-11 03:14:54 -0700

    Use fully-qualified class names

--- lib/between_meals/repo/git/cmd.rb
+++ lib/between_meals/repo/git/cmd.rb
@@ -18,8 +18,8 @@ require 'between_meals/cmd'
 
 module BetweenMeals
   class Repo
-    class Git < BetweenMeals::Repo
-      class Cmd < BetweenMeals::Cmd
+    class Git < ::BetweenMeals::Repo
+      class Cmd < ::BetweenMeals::Cmd
         def config(key)
           s = cmd("config #{key}", nil, true)
           unless [0, 1].include?(s.exitstatus)
